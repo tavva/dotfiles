7ec60404b85d54bececf164122e13b0a1eb2e788
Ben Phillips <ben@thread.com>
2017-10-16 11:15:11 +0100

    Use local theme.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -50,7 +50,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/ben/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
